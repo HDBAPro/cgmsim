1bac9ac990d622fa73ee69fccbaa2ad892e6e062
Lorenzo Sandini <lsandini@gmail.com>
2021-03-02 13:55:08 +0200

    Delete getfood.sh

--- getfood.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -H "Accept: application/json" -H "Content-Type: application/json" https://dmpkl-freeaps.herokuapp.com/api/v1/treatments/ -o treatments.json
-
-curl -H -s "Accept: application/json" -H "Content-Type: application/json" https://dmpkl-freeaps.herokuapp.com/api/v1/treatments/ | jq '.'
-
-wget 'https://dmpkl-freeaps.herokuapp.com/api/v1/treatments/' -O food.json;
-
-wget 'https://pplantnightscout.herokuapp.com/api/v1/treatments/' -O treatmentspaul.json;
