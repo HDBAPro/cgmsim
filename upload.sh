cf6a68cd8a1d2a76f82a138de0e2b6bfe2a1771a
Lorenzo Sandini <lsandini@gmail.com>
2021-03-03 13:18:03 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 node times.js;
 node bolusiob.js
-# curl -H "Content-Type: application/json" -H "api-secret:72ed4e79dec288f4b3e85466f1f92330771154ac" -XPOST 'https://dmpkl-freeaps.herokuapp.com/api/v1/entries/' -d @test-sgv.json
-curl -H "Content-Type: application/json" -H "api-secret:72ed4e79dec288f4b3e85466f1f92330771154ac" -XPOST 'https://dmpkl-freeaps.herokuapp.com/api/v1/entries/' -d @iobcalc-sgv.json
+# curl -H "Content-Type: application/json" -H "api-secret:72ed4e79d_YOUR_HASHED SECRET_f92330771154ac" -XPOST 'https://YOURNIGHTSCOUTHERE.herokuapp.com/api/v1/entries/' -d @test-sgv.json
+curl -H "Content-Type: application/json" -H "api-secret:72ed4e79d_YOUR_HASHED SECRET_f92330771154ac" -XPOST 'https://YOURNIGHTSCOUTHERE.herokuapp.com/api/v1/entries/' -d @iobcalc-sgv.json
