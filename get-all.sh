afdfda21646082e03924e6897c96fa40cd38ce56
Lorenzo Sandini <lsandini@gmail.com>
2021-03-03 13:16:20 +0200

    Update get-all.sh

--- get-all.sh
+++ get-all.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-curl -H "Accept: application/json" -H "Content-Type: application/json" https://dmpkl-freeaps.herokuapp.com/api/v1/entries/sgv/ | jq '.' > sgv.json;
+curl -H "Accept: application/json" -H "Content-Type: application/json" https://YOURNIGHTSCOUTHERE.herokuapp.com/api/v1/entries/sgv/ | jq '.' > sgv.json;
 
-curl -H "Accept: application/json" -H "Content-Type: application/json" https://dmpkl-freeaps.herokuapp.com/api/v1/treatments/ | jq '.' > entries.json;
+curl -H "Accept: application/json" -H "Content-Type: application/json" https://YOURNIGHTSCOUTHERE.herokuapp.com/api/v1/treatments/ | jq '.' > entries.json;
 
 node fetch-basal-compute-iob.js;
+
+node fetch-basalA-compute-iob.js;
 
 node fetch-mealtime-compute-iob.js;
 
