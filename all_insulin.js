f0897cfdcc5cf704aab972449fdb2058be40e34f
Lorenzo Sandini <lsandini@gmail.com>
2021-03-20 13:35:43 +0200

    Add files via upload

--- all_insulin.js
+++ all_insulin.js
@@ -1,3 +1,5 @@
+const ISF = 1.5;  //mmol/l/U
+
 var NR = require('./last_mealtime.json');
 var jsonNR = JSON.stringify(NR);
 var NRAct = JSON.parse(jsonNR);
@@ -16,12 +18,17 @@ let globalMealtimeAct = NRAct[0];
 
 let globlalInsulinAct = globalBasalAct + globalMealtimeAct;
 
+let BGI_ins = globlalInsulinAct *ISF * -5;
+
 console.log('-------------------------------------------');
 console.log('glaAct:',glaAct,'detAct:',detAct,'total basal act:', globalBasalAct);
 console.log('-------------------------------------------');
 console.log('total mealtime insulin activity:',globalMealtimeAct);
 console.log('-------------------------------------------');
 console.log('total insulin activity:',globlalInsulinAct);
+
+console.log('-------------------------------------------');
+console.log('total BG impact of insulin for 5 minutes:',BGI_ins);
 
 
 
