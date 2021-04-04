866620135cc967fa374393ba3cd0dc3a7fc4cfb0
Lorenzo Sandini <lsandini@gmail.com>
2021-04-04 20:03:59 +0300

    Add files via upload

--- get-jenni.sh
+++ get-jenni.sh
@@ -1,3 +1,5 @@
 curl -H "Accept: application/json" -H "Content-Type: application/json" https://munhaima.herokuapp.com/api/v1/treatments/ | jq '.' > jenni_treatments.json;
 curl -H "Accept: application/json" -H "Content-Type: application/json" https://munhaima.herokuapp.com/api/v1/profile.json | jq '.' > jenni_profile.json;
+node jenni.js;
+
 
