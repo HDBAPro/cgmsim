0de5fdca99f162934595ea4c25c8d1c79d2845a3
Lorenzo Sandini <lsandini@gmail.com>
2021-04-04 18:41:05 +0300

    Add files via upload

--- jenni.js
+++ jenni.js
@@ -2,6 +2,7 @@
 //===============================================================================
 
 
+const { floor } = require('mathjs');
 const jenni_profile = require('./jenni_profile.json');
 const jenni_treatments = require('./jenni_treatments.json');
 
@@ -33,9 +34,13 @@ const profileAutoSync = lastlist1.map(entry => entry.store).map(entry => ({ Open
 const basalProfileAutoSyncArr = profileAutoSync.map(entry => entry.OpenAPS_Autosync).map(entry => ({ basal: entry.basal}));
 //console.log('this is the array of basal in AutoSync profile:', basalProfileAutoSyncArr);
 
-const autoSyncProfile = basalProfileAutoSyncArr.map(entry => entry.basal);
-console.log('this is the latest AutoSync profile details:', autoSyncProfile);
+const autoSyncProfiles = basalProfileAutoSyncArr.map(entry => entry.basal);
+console.log('this is the latest AutoSync profile details:', autoSyncProfiles);
 
+
+const autoSyncProfile_z = autoSyncProfiles[0];
+var autoSyncProfile = autoSyncProfile_z.map(entry => ({ ...entry, time: parseFloat(entry.time), value: parseFloat(entry.value), timeAsSeconds: parseInt(entry.timeAsSeconds.slice(0))}));
+console.log('this is the trimmed down insulin and time since injection data:',autoSyncProfile); 
 
 
 
@@ -48,9 +53,57 @@ const allTempBasals = jenni_treatments.filter(e => e._id).map(e => ({duration: e
 let tempBasals = allTempBasals.filter(function (e) {
     return e.mills >= (Date.now()-10800000); // temps basals set in the last 3 hours
 });
-console.log('these are the tempBasals from the last 3 hours:', tempBasals);
+//console.log('these are the tempBasals from the last 3 hours:', tempBasals);
+
+let tempBasals2 = tempBasals.filter(function (e) {
+    return e.eventType = 'Temp Basal'; // temps basals set in the last 3 hours
+});
+console.log('these are the filtered tempBasals from the last 3 hours:', tempBasals2);
+
+let tempBasals3 = tempBasals2.filter(function (e) {
+    return e.duration != 0; // temps basals set in the last 3 hours
+});
+console.log('these are the filtered tempBasals from the last 3 hours:', tempBasals3);
+
 
 
 
 // Now let's start to compute the insulin from profile first and temp basals
 //==========================================================================
+let days = (Date.now() / 86400000);
+let relativeTimeOfDay = (days - Math.floor(days)) * 24 + 3;
+let relativeTimeOfDaySecs = ((days - Math.floor(days)) * 24 + 3) * 60 * 60;
+
+console.log('relative time of day since midnight in UTC time + 3h in hours:', relativeTimeOfDay);
+console.log('relative time of day since midnight in UTC time + 3h in seconds:', relativeTimeOfDaySecs);
+
+
+// let's detect the current basal from Autosync profile (TO DO : also minutes !)
+//==============================================================================
+const j = floor(relativeTimeOfDay);
+console.log(j);
+var currentAutoSyncBasal = autoSyncProfile[j].value;
+console.log('current basal according to AutoSync profile', currentAutoSyncBasal);
+
+// let's detect the current basal from Temp Basals
+//================================================
+const k = tempBasals3[1];
+var tempBasalEnd = tempBasals3[1].mills + tempBasals3[1].duration * 60 * 1000;
+var tempBasal = tempBasals3[1].absolute;
+console.log('current Temp Basal entry:',k);
+
+// if a temp basal is set, override the AutoSync Basal 
+//================================================
+if (Date.now() < tempBasalEnd) {
+    var currentTempBasal = tempBasal;
+
+} else {
+
+    currentTempBasal = currentAutoSyncBasal;
+};
+
+console.log('This is the final value for the basal rate now:',currentTempBasal)    
+
+
+
+
